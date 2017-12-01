87265bbfebb8e81a66259000f3e2dfe2ff338236
abdocode <113cooler>
2017-12-01 05:58:45 +0200

    code edit

---
+++ includes/templates/loginManagement.php
@@ -0,0 +1,18 @@
+<?php
+
+// Error Reporting
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+include 'DB_connect.php';
+
+// Routes
+$templates 	= 'includes/templates/'; // Templates Directory
+$functions	= '../functions/'; // Functions Directory
+$pages	= 'includes/pages/'; // Pages Directory
+$css 	= 'layout/css/'; // Css Directory
+$js 	= 'layout/js/'; // Js Directory
+
+// Include The Important Files
+include $functions . 'functions.php';
+include  'header.php';
